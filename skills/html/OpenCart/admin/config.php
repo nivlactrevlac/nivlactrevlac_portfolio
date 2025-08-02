626b0b4ef0cf174db531e4c93d85746de5638aee
nivlactrevlac <nivlactrevlac@gmail.com>
2025-08-02 19:23:45 -0400

    EspressoLane Upload pt1

---
+++ skills/html/OpenCart/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://sabal.uscb.edu/~calvertc/OpenCart/admin/');
+define('HTTP_CATALOG', 'http://sabal.uscb.edu/~calvertc/OpenCart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://sabal.uscb.edu/~calvertc/OpenCart/admin/');
+define('HTTPS_CATALOG', 'http://sabal.uscb.edu/~calvertc/OpenCart/');
+
+// DIR
+define('DIR_APPLICATION', '/home/calvertc/public_html/OpenCart/admin/');
+define('DIR_SYSTEM', '/home/calvertc/public_html/OpenCart/system/');
+define('DIR_IMAGE', '/home/calvertc/public_html/OpenCart/image/');
+define('DIR_LANGUAGE', '/home/calvertc/public_html/OpenCart/admin/language/');
+define('DIR_TEMPLATE', '/home/calvertc/public_html/OpenCart/admin/view/template/');
+define('DIR_CONFIG', '/home/calvertc/public_html/OpenCart/system/config/');
+define('DIR_CACHE', '/home/calvertc/public_html/OpenCart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/calvertc/public_html/OpenCart/system/storage/download/');
+define('DIR_LOGS', '/home/calvertc/public_html/OpenCart/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/calvertc/public_html/OpenCart/system/storage/modification/');
+define('DIR_UPLOAD', '/home/calvertc/public_html/OpenCart/system/storage/upload/');
+define('DIR_CATALOG', '/home/calvertc/public_html/OpenCart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'calvertc');
+define('DB_PASSWORD', '!QA2ws3ed4rf');
+define('DB_DATABASE', 'calvertc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
